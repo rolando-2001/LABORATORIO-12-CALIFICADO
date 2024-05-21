52fdbb4f8c51596ffa883cca7123448a5069bec8
rolando-2001 <rolando.casapaico@tecsup.edu.pe>
2024-05-21 14:47:23 -0500

    services

--- src/main/java/com/tecsup/petclinic/repositories/OwnerRepository.java
+++ src/main/java/com/tecsup/petclinic/repositories/OwnerRepository.java
@@ -1,10 +1,21 @@
 package com.tecsup.petclinic.repositories;
 
 import com.tecsup.petclinic.entities.Owner;
+import com.tecsup.petclinic.entities.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OwnerRepository extends JpaRepository<Owner,Integer> {
+import java.util.List;
 
+public interface OwnerRepository extends JpaRepository<Owner,Integer> {
+    List<Owner> findByName(String name);
+
+    List<Owner> findByTypeId(int typeId);
+
+    // Fetch  by ownerId
+    List<Owner> findByOwnerId(int ownerId);
+
+    @Override
+    List<Owner> findAll();
 
 
 }
--- src/main/java/com/tecsup/petclinic/services/OwerService.java
+++ src/main/java/com/tecsup/petclinic/services/OwerService.java
@@ -1,4 +1,22 @@
 package com.tecsup.petclinic.services;
 
+
+import com.tecsup.petclinic.entities.Owner;
+import com.tecsup.petclinic.entities.Pet;
+
 public interface OwerService {
+
+
+    Owner create(Owner owner);
+    Owner update(Owner Owner);
+
+
+
+
+
+
+
+
+
+
 }
